56c8a77f7af70afafa10c012e21b92e1ff20c4a3
Ihsan Faisal <ihsanfaisal@gmail.com>
2014-03-21 10:31:34 +0700

    Menambah konfigurasi smarty

--- konfigurasi.php
+++ konfigurasi.php
@@ -6,3 +6,8 @@ $passdb = '';
 $dbname = 'kota-kecamatan';
 
 $koneksidb = new PDO("mysql:host=$host;dbname=$dbname", $userdb, $passdb);
+
+require_once('libs/smarty/Smarty.class.php');
+$smarty = new Smarty();
+
+session_start();
