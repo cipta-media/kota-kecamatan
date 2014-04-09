9d83d32951492990c9503b83a46b4ddbea190176
Syeikh Anugrah <syeikhanugrah@gmail.com>
2014-04-09 21:04:20 +0700

    Merubah metode pengiriman email menggunakan PHPMailer

--- konfigurasi.php
+++ konfigurasi.php
@@ -2,12 +2,15 @@
 
 $host = 'localhost';
 $userdb = 'root';
-$passdb = '';
+$passdb = 'syeikhanugrah';
 $dbname = 'kota-kecamatan';
 
 $koneksidb = new PDO("mysql:host=$host;dbname=$dbname", $userdb, $passdb);
 
 require_once('libs/smarty/Smarty.class.php');
 $smarty = new Smarty();
+
+require_once('libs/PHPMailer/class.phpmailer.php');
+$mail = new PHPMailer();
 
 session_start();
